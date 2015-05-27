7f6a2b4700ed1a1f0c7fb5e5ca4fc515d325e211
Eyeball Edward <edwardyeung.private@gmail.com>
2015-05-27 10:58:45 +0800

    Files and fast forward again...

--- src/test/java/com/eyeball/utils/test/Main.java
+++ src/test/java/com/eyeball/utils/test/Main.java
@@ -1,13 +1,10 @@
 package com.eyeball.utils.test;
 
-<<<<<<< HEAD
 import java.io.File;
 import java.io.IOException;
 
 import com.eyeball.utils.io.FileTextList;
 import com.eyeball.utils.logging.Logger;
-=======
->>>>>>> 96edfa4cb1cddbd0710eedf27589a184de3b8888
 import com.eyeball.utils.logging.MultiLogger;
 import com.eyeball.utils.reflect.ReflectionHelper;
 
@@ -18,7 +15,6 @@ public class Main {
 	public static void main(String[] args) throws NoSuchFieldException,
 			SecurityException, ClassNotFoundException,
 			IllegalArgumentException, IllegalAccessException,
-<<<<<<< HEAD
 			NullPointerException, IOException {
 		// How to print? Everything is private.
 		// Use ReflectionHelper!
@@ -32,14 +28,12 @@ public class Main {
 			new Logger("File-Reader").info(s);
 		}
 
-=======
-			NullPointerException {
 		// How to print? Everything is private.
 		// Use ReflectionHelper!
-		LOGGER = ReflectionHelper.getField(References.class, /* static */null, "LOGGER");
+		LOGGER = ReflectionHelper.getField(References.class, /* static */null,
+				"LOGGER");
 		LOGGER.info("Hi?");
 		LOGGER.info("Yes! this worked!");
->>>>>>> 96edfa4cb1cddbd0710eedf27589a184de3b8888
 	}
 
 }
